e7a382eae94c618bcea78a985e7cd60738dd9fd4
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-05-31 13:06:22 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -8,7 +8,7 @@ storefront.on('widget:@ecomplus/widget-tag-manager', () => {
         }
         const $points = document.querySelector('.prices__points')
         let points = document.querySelector('.prices__points span').innerText.replace('+', '').trim()
-        let transformToMoney = (Number(points) * 0.05).toFixed(2)
+        let transformToMoney = (Number(points) * 0.15).toFixed(2)
         const $money =  `<div id="dinheiro-volta"><span><strong>Ganhe</strong> <span class="dinheiro">R$ <strong id="changeMoney">${transformToMoney}</strong></span> <br> em cashback na sua próxima compra</span></div>`
         const $div = document.createElement('div')
         $div.id = 'cashback'
