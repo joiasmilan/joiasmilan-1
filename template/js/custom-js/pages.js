9acf9f34f5aa95b6fe5219d0a10399628062cd6e
matheusgnreis <matheus@e-com.club>
2022-10-24 16:35:46 -0300

    chore: insert points on product page

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,15 @@
 // Add your custom JavaScript for storefront pages here.
+if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
+    storefront.on('widget:@ecomplus/widget-tag-manager', function () {
+      setTimeout(function () {
+        const $points = document.querySelector('.product__prices .prices__points')
+        let points = document.querySelector('.product__prices .prices__points span').innerText.replace('+', '').trim()
+        let transformToMoney = (Number(points) * 0.05).toFixed(2)
+        const $money =  `<div id="dinheiro-volta"><span>Ganhe <span class="dinheiro"><strong nid="changeMoney">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transformToMoney)}</strong></span> na próxima compra</span></div>`
+        $('.product__buy').after($money)
+        if ($('.product__kit').length) {
+          $('.product__kit').after($money)
+        }
+      }, 500);
+    });
+  }
